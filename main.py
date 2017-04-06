050ab323541a9757a5a9993b0f10c1c15ac60165
Lonami Exo <totufals@hotmail.com>
2017-04-06 18:54:51 +0200

    Fix client not disconnecting after the window is closed

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ from utils import get_cached_client
 
 if __name__ == '__main__':
     # Since this is the first time, it will be loaded and cached
-    get_cached_client()
-    start_app(SelectDialogWindow)
+    try:
+        get_cached_client()
+        start_app(SelectDialogWindow)
+    finally:
+        get_cached_client().disconnect()
